6e5285dc81efbe9b705dd9c63859edbc61374a97
ichsanachmad <ichsanachmad33@gmail.com>
2018-07-04 19:15:06 +0700

    Deploy

---
+++ public/script.js
@@ -0,0 +1,200 @@
+$(function () {
+    var socket = io.connect();
+    var $messageForm = $('#messageForm');
+    var $message = $('#message');
+    var $chat = $('#chat');
+    var $messageArea = $('#messageArea');
+    var $loginForm = $('#loginForm');
+    var $registerForm = $('#registerForm');
+    var $userFormArea = $('#userFormArea');
+    var $users = $('#users');
+    var $loginUsername = $('#login-username');
+    var $loginPassword = $('#login-password');
+    var $registerUsername = $('#reg-username');
+    var $registerPassword = $('#reg-password');
+    var $registerNickname = $('#reg-nickname');
+    var $userLogout = $('#userLogout');
+    var $my3 = $('#my-3');
+    var currentUser = '';
+
+
+    $messageForm.submit(function (e) {
+      e.preventDefault();
+      if($message.val()!=''){
+        socket.emit('ini pesan', currentUser);
+        socket.emit('send message', $message.val(),function(data){
+          //stuff
+        });
+        $message.val('');
+      }
+      else{
+         
+      }
+      
+    });
+
+    // socket.on('whisper',function(data){
+    //   if (currentUser == data.temp) {
+    //     $chat.append('<div class="chat-msg mb-2 px-3 py-2 bg-white">'+ data.msg + '</div><br>');
+    //   } else {
+    //     $chat.append('<div class="chat-msg mb-2 px-3 py-2 btn-primary receiver clear"><strong>' + data.user + ' : </strong> ' + data.msg + '</div><br>');
+    //   }
+    //   autoScroll();
+    // });
+
+    socket.on('new message', function (data) {
+      if (currentUser == data.temp) {
+        $chat.append('<div class="chat-msg mb-2 px-3 py-2 bg-white">'+ data.msg + '</div><br>');
+      } else {
+        $chat.append('<div class="chat-msg mb-2 px-3 py-2 btn-primary receiver clear"><strong>' + data.user + ' : </strong> ' + data.msg + '</div><br>');
+      }
+      autoScroll();
+    });
+
+    $("#message").keypress(function (e) {
+      if (e.which == 13 && !e.shiftKey) {
+        $(this).closest("form").submit();
+        e.preventDefault();
+        return false;
+      }
+    });
+
+    $loginForm.submit(function (e) {
+      e.preventDefault();
+      if (!isEmpty()) {
+        socket.emit('login', $loginUsername.val(), $loginPassword.val(), function (data, status) {
+          if (status == true) {
+            alert("You Logged In as "+data);
+            $userFormArea.hide();
+            $my3.show();
+            $userLogout.show();
+            $messageArea.show();
+            $('#firstHeader').hide();
+            $('#firstHeaderH4').hide();
+            currentUser = data;
+
+            socket.on('user joined', (data) => {
+              $chat.append('<center><div class="chat-msg mb-2 px-3 py-2 bg-white">'+ data.username + ' JOINED</div></center><br>');
+              autoScroll();
+             });
+
+          }
+          else {
+            alert("User Not Found!");
+          }
+        });
+      }
+    });
+
+    socket.on('user out', (data) => {
+      $chat.append('<center><div class="chat-msg mb-2 px-3 py-2 bg-white">'+ data.username + ' Logged Out</div></center><br>');
+      autoScroll();
+    });
+
+    $registerForm.submit(function (e) {
+      e.preventDefault();
+      if (!isEmpty()) {
+        socket.emit('register', $registerNickname.val(), $registerUsername.val(), $registerPassword.val(), function (status) {
+          if (status) {
+            $registerNickname.val('');
+            $registerUsername.val('');
+            $registerPassword.val('');
+            alert("Register Successfully");
+          }
+          else {
+            alert("Username already taken!");
+          }
+        });
+      }
+      
+    });
+
+    socket.on('get users', function (data) {
+      var html = '';
+      for (var i = 0; i < data.length; i++) {
+        if (data) {
+          html += '<li class="list-group-item">' + data[i] + '</li>'
+            ;
+        }
+      }
+      $users.html(html);
+    });
+
+    $('a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
+      $('a[data-toggle="pill"]').removeClass("active");
+      $(this).addClass("active");
+      var id = $(this).data("id");
+      currentTab = id;
+    });
+  });
+  
+  var currentTab = 'signin';
+
+  function autoScroll() {
+    var elem = document.getElementById('chat');
+    elem.scrollTop = elem.scrollHeight;
+  }
+
+  function userLogout() {
+    
+    location.reload();
+   
+  }
+
+  function showTime() {
+    var d = new Date();
+    return d.getHours() + '.' + (d.getMinutes()<10?0:'') + d.getMinutes();
+  }
+
+  function isEmpty(){
+    var loginUser = document.getElementById('login-username');
+    var loginPass = document.getElementById('login-password');
+    var regUser = document.getElementById('reg-username');
+    var regPass = document.getElementById('reg-password');
+    var regNick = document.getElementById('reg-nickname');
+
+    if (currentTab == 'signin') {
+      if (loginUser.value.trim() != '') {
+        loginUser.style.borderColor = "#ced4da";
+      } else if (loginPass.value.trim() != '') {
+        loginPass.style.borderColor = "#ced4da";
+      }
+
+      if (loginUser.value.trim() == '' && loginPass.value.trim() == '') {
+        loginUser.style.borderColor = "#FF0000";
+        loginPass.style.borderColor = "#FF0000";
+        return true;
+      } else if (loginUser.value.trim() == '') {
+        loginUser.style.borderColor = "#FF0000";
+        return true;
+      } else if (loginPass.value.trim() == '') {
+        loginPass.style.borderColor = "#FF0000";
+        return true;
+      }
+      return false;
+    } else {
+      if (regUser.value.trim() != '') {
+        regUser.style.borderColor = "#ced4da";
+      } else if (regPass.value.trim() != '') {
+        regPass.style.borderColor = "#ced4da";
+      } else if (regNick.value.trim() != '') {
+        regNick.style.borderColor = "#ced4da";
+      }
+
+      if (regUser.value.trim() == '' && regPass.value.trim() == '' && regNick.value.trim() == '') {
+        regUser.style.borderColor = "#FF0000";
+        regPass.style.borderColor = "#FF0000";
+        return true;
+      } else if (regUser.value.trim() == '') {
+        regUser.style.borderColor = "#FF0000";
+        return true;
+      } else if (regPass.value.trim() == '') {
+        regPass.style.borderColor = "#FF0000";
+        return true;
+      } else if (regNick.value.trim() == '') {
+        regNick.style.borderColor = "#FF0000";
+        return true;
+      }
+      return false;
+    }
+  }
--- server.js
+++ server.js
@@ -1,49 +1,159 @@
 var express = require('express');
 var app = express();
-var server =  require('http').createServer(app);
+var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-users=[];
-connections=[];
-
-server.listen(process.env.PORT || 3000);
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "iochat"
+});
+var userTemp = '';
+users = [];
+connections = [];
+var output = false;
+server.listen(process.env.PORT || 36676);
 console.log('Server running');
-app.get('/',function(req,res){
+app.use("/public", express.static(__dirname + "/public"));
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected!');
+});
 
-
-io.sockets.on('connection',function(socket){
+io.sockets.on('connection', function (socket) {
   connections.push(socket);
   console.log('Connected : %s sockets connected', connections.length);
 
   //Disconnectnya
-  socket.on('disconnect',function(data){
-
-    users.splice(users.indexOf(socket.username),1);
+  socket.on('disconnect', function (data) {
+    users.splice(users.indexOf(socket.username), 1);
     updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected : %s sockets connected', connections.length);
+    
+    socket.broadcast.emit('user out', {
+      username: socket.username
+    });
+    
+  });
+
+  socket.on('ini pesan', function (data) {
+    userTemp = data;
   });
 
   //sendMessage
+  socket.on('send message', function (data,callback) {
+      // var msg = data.trim();
+      // var whispUser='';
+      // if(msg.substr(0,3) === '/w '){
+      //   msg = msg.substr(3);
+      //   var ind = msg.indexOf(' ');
+      
+      //   if(ind != -1){
+      //     console.log('ini whisper 2');
+      //       var name = msg.substring(0, ind);
+      //       var msgTemp =  msg.substring(ind + 1);
+      //       for(var i =0 ; i<users.length;i++){
+      //         if(name==users[i]){
+      //           socket.emit('whisper', {
+      //             msg: msgTemp,
+      //             user: socket.username,
+      //             temp: userTemp
+      //           });
+      //           callback(msg);  
+      //           userTemp = '';
+      //           console.log('ini whisper 3');
+      //           break;
+      //         }else{
+      //           console.log('ini salah');
+      //           console.log(name);
+      //           console.log(users);
+      //           console.log();
+      //           break;
+                
+      //         }
+      //       }  
+      //   }else{
+      //     callback('error, silahkan masukan pesan whisper');
+      //   }
+        
+      // }else{
+        io.sockets.emit('new message', {
+          msg: data,
+          user: socket.username,
+          temp: userTemp
+        });
+        userTemp = '';
+      // }
+  });
+  
 
-  socket.on('send message', function(data){
-    console.log(data);
-    io.sockets.emit('new message', {msg: data, user:socket.username});
+  //Login
+  socket.on('login', function (username, password, callback) {
+    var sql = "SELECT username, password, nickname FROM users";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      for (var i = 0; i < result.length; i++) {
+        if (username == result[i].username && password == result[i].password) {
+          callback(result[i].nickname, true);
+          socket.username = result[i].nickname;
+          users.push(socket.username);
+          updateUsernames();
+          console.log(username + ' has logged in');
+          socket.broadcast.emit('user joined', {
+            username: socket.username
+          });
+          break;
+        } else {
+          if (i == (result.length - 1)) {
+            callback('', false);
+            console.log('Failed to login');
+          }
+        }
+      }
+    });
+
+      
   });
 
-  //new users
-  socket.on('new user', function(data, callback){
-    callback(data);
-    socket.username=data;
-    users.push(socket.username);
-    updateUsernames();
+ //logout
+ 
+
+  //Register
+  socket.on('register', function (nickname, username, password, callback) {
+    var sqlCheck = "SELECT username FROM users";
+    con.query(sqlCheck, function (err, result) {
+      if (err) throw err;
+      for (var i = 0; i < result.length; i++) {
+        if (username == result[i].username) {
+          console.log('Username is Taken');
+          callback(false);
+        }else if(nickname==result[i].nickname){
+          console.log('Nickname is Taken');
+          callback(false);
+        }else{
+          if (i == (result.length - 1)) {
+            var sqlRegister = "INSERT INTO users (username, password, nickname) VALUES ?";
+            var values = [[username, password, nickname]];
+            con.query(sqlRegister, [values], function (err, result) {
+              if (err) throw err;
+              console.log(username + ' has Successfully Registered');
+            });
+            callback(true);
+          }
+        }
+      }
+    });
   });
 
-  function updateUsernames(){
-    
-    io.sockets.emit('get users',users);
-
+  
+  
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
   }
 });
